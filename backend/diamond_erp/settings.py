44d3b0961d12df161999b019aa76d5a57699bcfa
meetzadafiya1075 <meet.zadafiya1075@gmail.com>
2026-04-30 19:04:27 +0530

    Allow all hosts to resolve 400 Bad Request error

--- backend/diamond_erp/settings.py
+++ backend/diamond_erp/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-a)3sa)#2$-d^60je(l(6#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 
