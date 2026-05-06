cc42d6493902ac3110fd1f065b8b5114f1ead159
meetzadafiya1075 <meet.zadafiya1075@gmail.com>
2026-05-06 14:25:01 +0530

    Set Cloudinary resource type to auto and modernize storage config

--- backend/diamond_erp/settings.py
+++ backend/diamond_erp/settings.py
@@ -128,7 +128,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'RESOURCE_TYPE': 'auto'
 }
 
 STORAGES = {
